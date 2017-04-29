493cfe393e89391e881c0f6bf649b589e346aecc
Samuel Stanton <samuel.stanton@ucdenver.edu>
2017-04-29 02:29:27 -0600

    New Version

--- Source.cpp
+++ Source.cpp
@@ -26,11 +26,6 @@ const int HOSTNUM = 302;
 
 const int COUNTRYNUM = 28;
 
-// Objective weights alpha:preference, beta:balance
-double alpha = .0001;
-
-double beta = .9999;
-
 
 struct edge 
 {
@@ -55,7 +50,7 @@ struct shortest_path {
 vector<vector<string> > readIn(vector<vector<string> > vect, string filename);
 
 // Functions to construct graphs and adjacency lists
-vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localbalance[HOSTNUM], double localcapacity[HOSTNUM]);
+vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localbalance[HOSTNUM], double localcapacity[HOSTNUM], double alpha, double beta);
 
 vector<edge> create_residual(vector<edge> localgraph);
 
@@ -63,35 +58,71 @@ vector<vector<int> > makeAdjList(vector<edge> localgraph);
 
 
 // Successive Shortest Path and Dijkstra
-vector<double> ssp_mincost(vector<edge> localgraph, double localgrant_totals[SOURCENUM]);
+vector<double> ssp_mincost(vector<edge> localgraph, double localapptotals[SOURCENUM]);
 
 shortest_path dijkstra(int start_node, int end_node, vector<edge> localgraph, vector< vector<int> > localadj_list);
 
 
-double objFuncVal(vector<edge> localgraph, vector<double> localflow);
+double objFuncVal(vector<edge> localgraph, vector<double> localflow, double alpha, double beta);
 
-double gradient(vector<edge> localgraph, vector<double> localflow, int index);
+double gradient(vector<edge> localgraph, vector<double> localflow, int index, double alpha, double beta, double mean);
 
 double meanBurden(vector<edge> localgraph, vector<double> localflow);
 
-double prefVal(vector<edge> localgraph, vector<double> localflow);
+double prefVal(vector<edge> localgraph, vector<double> localflow, double alpha, double beta);
 
 double balanceVal(vector<edge> localgraph, vector<double> localflow);
 
 
-double linesearch(vector<edge> localgraph, vector<double> localdescent, vector<double> localflow, vector<double> localdirection);
+double linesearch(vector<edge> localgraph, vector<double> localdescent, vector<double> localflow, vector<double> localdirection, double alpha, double beta);
 
 double lnSrchFncA(vector<edge> localgraph, vector<double> localflow, int index);
 
-double lnSrchFncB(vector<edge> localgraph, vector<double> localflow, vector<double> localdescent);
+double lnSrchFncB(vector<edge> localgraph, vector<double> localflow, vector<double> localdescent, double alpha, double beta);
 
-double lnSrchFncC(vector<edge> localgraph, vector<double> localdescent);
+double lnSrchFncC(vector<edge> localgraph, vector<double> localdescent, double alpha, double beta);
+
+
+void solver(double alpha, double beta);
 
 
 int main()
+{		
+	// Main routine, writes data to output file
+	
+	for (int i = 0; i < 21; i++)
+	{
+		// Objective weights: alpha for preference, beta for balance
+		double currentalpha = (double) i / 20;
+
+		double currentbeta = 1 - currentalpha;
+
+		solver(currentalpha, currentbeta);
+	}
+
+	//solver(0, 1);
+
+	//solver(0.5, 0.5);
+
+	//solver(0.9, 0.1);
+
+	// solver(1, 0);
+
+	cin.get();
+	cin.get();
+	
+}
+
+void solver(double alpha, double beta)
 {
 	vector<vector<string> > cities, asylumapps, asylumgranted;
 
+	fstream objVals;
+	fstream	appAssignments;
+
+	objVals.open("PA-objValsE-00.txt", ios::app);
+
+	appAssignments.open("PA-assignmentsE-00.txt", ios::app);
 
 	// Specify filepaths
 	cities = readIn(cities, "CityPop200K.csv");
@@ -100,28 +131,40 @@ int main()
 
 	asylumgranted = readIn(asylumgranted, "AsylumDecisionsFirstTime.csv");
 
+	
+	int countryID = 0;
+
+	vector<double> countryflow(COUNTRYNUM, 0);
 
 	//costs
-	double pref[HOSTNUM][SOURCENUM] = {};
-	
-	double balance[HOSTNUM] = {};
-	
+	double pref[HOSTNUM][SOURCENUM] = {0};
+
+	double balance[HOSTNUM] = {0};
+
 
 	// edge capacity
-	double capacity[HOSTNUM] = {};
-	
-	double country_pref[COUNTRYNUM][SOURCENUM] = {};
-	
-	double app_array[COUNTRYNUM][SOURCENUM] = {};
-	
-	double grant_array[COUNTRYNUM][SOURCENUM] = {};
-	
-	double app_totals[SOURCENUM] = {};
-	
-	double grant_totals[SOURCENUM] = {};
+	double capacity[HOSTNUM] = {0};
+
+	double country_pref[COUNTRYNUM][SOURCENUM] = {0};
+
+	double app_array[COUNTRYNUM][SOURCENUM] = {0};
+
+	double grant_array[COUNTRYNUM][SOURCENUM] = {0};
+
+	double app_totals[SOURCENUM] = {0};
+
+	double grant_totals[SOURCENUM] = {0};
+
+	double scaleTerm = 5*pow(10, 6);
+
+	double tol = 1;		// Define exit tolerance;
+
+	double capTotal = 0;
 
 
 	// Extracts data from vector strings, places into arrays
+
+
 	for (int j = 0; j < COUNTRYNUM; j++)
 	{
 		for (int i = 0; i < SOURCENUM; i++)
@@ -133,133 +176,229 @@ int main()
 		}
 
 	}
-	
-	
+
+
+
 	for (int j = 0; j < COUNTRYNUM; j++)
 	{
 		for (int i = 0; i < SOURCENUM; i++) { country_pref[j][i] = app_array[j][i] / app_totals[i]; }
 	}
 
-	
-	int skip = 1;
-	for (int j = 1; j < cities.size() - 1; j++)
-	{
-		int tempid = atoi(cities[j][2].c_str());
 
-		if (tempid == 0) 
-		{ 
-			skip = skip + 1;
-			continue; 
+	vector<int> changeOfCountry(COUNTRYNUM - 1, 0);
+
+	vector<string> countryName(COUNTRYNUM);
+
+	vector<double> countryUrbPop(COUNTRYNUM, 0);
+	
+	int j = 0;
+	int country = 0;
+
+	while (j < HOSTNUM)
+	{
+		int tempid = atoi(cities[j+country+1][2].c_str());
+
+		if (tempid == 0)
+		{
+			countryName[country] = cities[j + country + 1][0];
+
+			if (j != 0) { changeOfCountry[country-1] = j; }
+
+			country++;
+
+			continue;
 		}
 
 		for (int i = 0; i < SOURCENUM; i++)
 		{
-			pref[j-skip][i] = country_pref[tempid - 1][i];
+			pref[j][i] = country_pref[country - 1][i] / scaleTerm;
 		}
 
-		capacity[j-skip] = atof(cities[j][1].c_str()) / 100;
+		double urbPop = atof(cities[j + country + 1][1].c_str());
+		
+		capacity[j] = urbPop / 100 ;
+
+		capTotal = capTotal + capacity[j];
+
+		countryUrbPop[country - 1] = countryUrbPop[country - 1] + urbPop;
+
+		j++;
 	}
 
 	
+
 	// Create network
-	vector<edge> graph = create_network(pref, balance, capacity);
-	
+	vector<edge> graph = create_network(pref, balance, capacity, alpha, beta);
+
 	vector<edge> staticgraph = graph;
 
-	
+
 	// Create adjacency list
 	vector<vector<int> > adj_list = makeAdjList(graph);
 
-	
+
 	// Set start time for runtime calculation
 	clock_t startTime = clock();
-	
+
 	double runTime;
-	
 
-	// Initialize flow
-	vector<double> flow = ssp_mincost(graph, grant_totals);
 
+	// Initialize flow with perfectly balanced flow to improve solving time
 	
+	vector<double> flow(graph.size(), 0);
+
+	int offset = HOSTNUM * SOURCENUM;
+
+	for (int j = 0; j < HOSTNUM; j++)
+	{
+		for (int i = 1 ; i < SOURCENUM; i++)
+		{
+			flow[j + offset] = flow[j + offset] + app_totals[i] * capacity[j] / capTotal;
+		}
+		
+	}
+
+	for (int j = 0; j < HOSTNUM; j++)
+	{
+		for (int i = 0; i < offset; i++)
+		{
+			if (graph[i].tail == j)
+			{
+				flow[i] = app_totals[graph[i].head] * capacity[j] / capTotal;
+			}
+		}
+	}
+	//
+	//for (unsigned int edge = 0; edge < offset; edge++)
+	//{
+	//for (int i = 0; i < SOURCENUM; i++)
+	//{
+	//if (graph[edge].head == i)
+	//{
+	//flow[edge] = app_totals[i] / (double)HOSTNUM;
+	//}
+	//}
+	//}
+
+	//for (unsigned int edge = 0; edge < offset; edge++)
+	//{
+	//for (int j = 0; j < HOSTNUM; j++)
+	//{
+	//if (graph[edge].tail == j)
+	//{
+	//flow[j+offset] = flow[j+offset] + flow[edge];
+	//}
+	//}
+	//}
+
+
 	// Begin main loop
-	double tol = 0.001; 	// Define exit tolerance;
 
-	
-	for (int rep = 0; true; rep++) 
+
+
+	for (int rep = 0; true; rep++)
 	{
 		if (rep % 100 == 99) { cout << '.'; }
-		
-	
+
+
 		// Update graph balance edge costs with gradient
-		for (int b_edge = 0; b_edge < HOSTNUM; b_edge++) 
+		double mean = meanBurden(graph, flow);
+
+		for (int b_edge = 0; b_edge < HOSTNUM; b_edge++)
 		{
-			graph[b_edge + HOSTNUM*SOURCENUM].cost = gradient(graph, flow, b_edge);
+			graph[b_edge + offset].cost = gradient(graph, flow, b_edge, alpha, beta, mean);
 		}
 
-		
+
 		// Solve min cost on updated graph to find improving direction
-		vector<double> direction = ssp_mincost(graph, grant_totals);
-		
+		vector<double> direction = ssp_mincost(graph, app_totals);
+
 		vector<double> descent;
-		
+
 		for (unsigned int i = 0; i < flow.size(); i++) { descent.push_back(direction[i] - flow[i]); }
 
-		
-		// Line search for optimal step size
-		double stepsize = linesearch(staticgraph, descent, flow, direction);
 
-		
+		// Line search for optimal step size
+		double stepsize = linesearch(staticgraph, descent, flow, direction, alpha, beta);
+
+
 		// Update flow
 		vector<double> oldflow = flow;
-		
+
 		double tempsum = 0;
-		
-		for (unsigned int i = 0; i < flow.size(); i++) 
+
+		for (unsigned int i = 0; i < flow.size(); i++)
 		{
 			flow[i] = flow[i] + stepsize*(descent[i]);
-			
+
 			tempsum = tempsum + pow((oldflow[i] - flow[i]), 2);
 		}
 
-		
+
 		// Check stopping criteria
-		if (sqrt(tempsum) < tol) 
-		{ 
+		if (sqrt(tempsum) < tol)
+		{
 			runTime = (clock() - startTime) / CLOCKS_PER_SEC;
-			
+
 			cout << "\nAlgorithm has converged in " << runTime << " seconds after " << rep << " iterations. \n";
-			
-			break; 
+
+			break;
 		}
-	
-		else if (rep > pow(10, 6)) 
-		{ 
+
+		else if (rep > pow(10, 6))
+		{
 			cout << "\n Algorithm failed to converge. \n";
-		
-			exit(-1); 
+
+			exit(-1);
 		}
 	}
 
+	setprecision(12);
+
+	//Aggregate city flows back to country flows
 	
-	cout << "Preference score: " << prefVal(staticgraph, flow) << endl;
-	
-	cout << "Variance score: " << balanceVal(staticgraph, flow) << endl;
-	
-	cout << "Mean Burden: " << meanBurden(staticgraph, flow) << endl;
 
 	
-	cin.get();
-	cin.get();
-	
+	for (int i = 0; i < HOSTNUM; i++)
+	{
+		if (i == changeOfCountry[countryID])
+		{
+			countryID++;
+		}
+		
+		countryflow[countryID] = countryflow[countryID] + flow[i + HOSTNUM*SOURCENUM];
+	}
+
+	//cout << "Country		Urban Pop.		Asylum Apps Assigned\n";
+	for (int i = 0; i < COUNTRYNUM; i++)
+	{
+		appAssignments << countryName[i] << ',' << countryUrbPop[i] << ',' << round(countryflow[i]) << endl;
+	}
+
+	appAssignments << endl;
+
+	appAssignments.close();
+
+	cout << "alpha: " << alpha << endl;
+
+	cout << "Preference score: " << prefVal(staticgraph, flow, alpha, beta) << endl;
+
+	cout << "Variance score: " << balanceVal(staticgraph, flow) << endl;
+
+	cout << "Objective function value " << objFuncVal(staticgraph, flow, alpha, beta) << endl;
+
+	objVals << alpha << ',' << prefVal(staticgraph, flow, alpha, beta) << ',' << balanceVal(staticgraph, flow) << ',' << objFuncVal(staticgraph, flow, alpha, beta) << ',' << runTime << endl;
+
+	objVals.close();
+
+	return;
 }
 
-double gradient(vector<edge> localgraph, vector<double> localflow, int index) 
+double gradient(vector<edge> localgraph, vector<double> localflow, int index, double alpha, double beta, double mean) 
 {
 	double gradval = 0, temp1 = 0, temp2 = 0, R = (double)HOSTNUM;
 	
 	int offset = HOSTNUM * SOURCENUM;
-
-	double mean = meanBurden(localgraph, localflow);
 
 
 	for (int node1 = 0; node1 < HOSTNUM; node1++) 
@@ -306,7 +445,7 @@ double lnSrchFncA(vector<edge> localgraph, vector<double> localflow, int index)
 	return meanBurden(localgraph, localflow) - localflow[index + HOSTNUM*SOURCENUM] / localgraph[index + HOSTNUM*SOURCENUM].cap; 
 }
 
-double lnSrchFncB(vector<edge> localgraph, vector<double> localflow, vector<double> localdescent) 
+double lnSrchFncB(vector<edge> localgraph, vector<double> localflow, vector<double> localdescent, double alpha, double beta) 
 {
 	double temp = 0, temp2 = 0;
 
@@ -330,7 +469,7 @@ double lnSrchFncB(vector<edge> localgraph, vector<double> localflow, vector<doub
 	return temp + temp2;
 }
 
-double lnSrchFncC(vector<edge> localgraph, vector<double> localdescent) 
+double lnSrchFncC(vector<edge> localgraph, vector<double> localdescent, double alpha, double beta)
 {
 	double temp = 0;
 	
@@ -346,36 +485,36 @@ double lnSrchFncC(vector<edge> localgraph, vector<double> localdescent)
 	return temp;
 }
 
-double linesearch(vector<edge> localgraph, vector<double> localdescent, vector<double> localflow, vector<double> localdirection) 
+double linesearch(vector<edge> localgraph, vector<double> localdescent, vector<double> localflow, vector<double> localdirection, double alpha, double beta)
 {
 	double lambda = 1, temp = 0;
 
-	temp = -lnSrchFncB(localgraph, localflow, localdescent) / lnSrchFncC(localgraph, localdescent);
+	temp = -lnSrchFncB(localgraph, localflow, localdescent, alpha, beta) / lnSrchFncC(localgraph, localdescent, alpha, beta);
 
 	
 	if (temp > 0 && temp < 1) { lambda = temp; }
 
 	
-	else if (objFuncVal(localgraph, localdirection) > objFuncVal(localgraph, localflow)) { lambda = 0; }
+	else if (objFuncVal(localgraph, localdirection, alpha, beta) > objFuncVal(localgraph, localflow, alpha, beta)) { lambda = 0; }
 
 	
 	return lambda;
 }
 
-double objFuncVal(vector<edge> localgraph, vector<double> localflow) 
+double objFuncVal(vector<edge> localgraph, vector<double> localflow, double alpha, double beta)
 {
 	double temp1 = 0, temp2 = 0;
 	
 	int offset = HOSTNUM * SOURCENUM;
 	
-	temp1 = alpha * prefVal(localgraph, localflow);
+	temp1 = alpha * prefVal(localgraph, localflow, alpha, beta);
 	
 	temp2 = beta * balanceVal(localgraph, localflow);
 	
 	return temp1 + temp2;
 }
 
-double prefVal(vector<edge> localgraph, vector<double> localflow) 
+double prefVal(vector<edge> localgraph, vector<double> localflow, double alpha, double beta)
 {
 	double temp = 0;
 	
@@ -384,7 +523,7 @@ double prefVal(vector<edge> localgraph, vector<double> localflow)
 	
 	for (int i = 0; i < offset; i++) 
 	{
-		temp = temp + localgraph[i].cost * localflow[i] / alpha;
+		temp = temp + localgraph[i].cost * localflow[i];
 	}
 	
 	
@@ -409,7 +548,7 @@ double balanceVal(vector<edge> localgraph, vector<double> localflow)
 	return variance;
 }
 
-vector<double> ssp_mincost(vector<edge> localgraph, double localgrant_totals[SOURCENUM]) 
+vector<double> ssp_mincost(vector<edge> localgraph, double localapptotals[SOURCENUM]) 
 {
 	vector<edge> residualgraph = create_residual(localgraph);
 	
@@ -444,7 +583,7 @@ vector<double> ssp_mincost(vector<edge> localgraph, double localgrant_totals[SOU
 	{
 		excessNodes.push_back(i);
 		
-		excessAmt.push_back(localgrant_totals[i]);
+		excessAmt.push_back(localapptotals[i]);
 		
 		sinkAmt = sinkAmt - excessAmt[i];
 	}
@@ -660,7 +799,7 @@ shortest_path dijkstra(int start_node, int end_node, vector<edge> localgraph, ve
 	return s_path;
 }
 
-vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localbalance[HOSTNUM], double localcapacity[HOSTNUM]) 
+vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localbalance[HOSTNUM], double localcapacity[HOSTNUM], double alpha, double beta)
 {	
 	// Initialize graph as vector of edges
 	vector<edge> localgraph;
@@ -674,7 +813,7 @@ vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localba
 		{
 			localgraph.push_back(edge());
 			
-			localgraph[count].cost = alpha * -localpref[j][i];
+			localgraph[count].cost = (-1)*localpref[j][i];
 			
 			localgraph[count].cap = localcapacity[j];
 			
@@ -692,7 +831,7 @@ vector<edge> create_network(double localpref[HOSTNUM][SOURCENUM], double localba
 	{
 		localgraph.push_back(edge());
 		
-		localgraph[count].cost = beta * localbalance[j];
+		localgraph[count].cost = localbalance[j];
 		
 		localgraph[count].cap = localcapacity[j];
 		
